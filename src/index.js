6c5389a5aba3cec8bf895a40eb4b5d895304d5f2
GemaBarrera <gbarrerafront@gmail.com>
2022-02-06 17:55:48 +0100

    firebase config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const firebase = require('firebase/app');
+
+const firebaseKey = process.env.FIREBASE_API_KEY;
+
+const firebaseConfig = {
+  apiKey: firebaseKey,
+  authDomain: "web-kids-activity.firebaseapp.com",
+  projectId: "web-kids-activity",
+  storageBucket: "web-kids-activity.appspot.com",
+  messagingSenderId: "402516340590",
+  appId: "1:402516340590:web:8c0d95bf0a4b3f8aa25b7f"
+}
+
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
   <React.StrictMode>
     <App />
